1b3222d4e36d68d8fb8f79cf7b898512adb85029
alfana21 <alff21r@gmail.com>
2018-09-03 21:25:20 -0500

    “mensaje”

--- resources/views/plantilla/artistas.blade.php
+++ resources/views/plantilla/artistas.blade.php
@@ -4,25 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Lista de ##### </title>
+    <title>Lista de Artistas </title>
 </head>
 <body>
     
     <center>
     <table border=1>
         <tr>
-            <td colspan=2><center><b> ##### </b</center></td>
+            <td colspan=2><center><b> Mis artistas </b</center></td>
         </tr>
 
         
         <tr>
-            <td><b> ##### </b></td>
-            <td> ##### </td>
+            <td><b> Artista 1 </b></td>
+            <td><b> Artista 2 </b></td>
+            <td><b> Artista 3 </b></td>
+            <td><b> Artista 4 </b></td>
+
+            <td> Katy Perry </td>
+            <td> Enrique Iglesias </td>
+            <td> Katy Perry </td>
         </tr>
         
 
         <tr>
-            <td colspan=2><center><b> ##### </b</center></td>
+            <td colspan=2><center><b> Quiz Primer Corte </b</center></td>
         </tr>
     </table>
     </center>
--- resources/views/plantilla/canciones.blade.php
+++ resources/views/plantilla/canciones.blade.php
@@ -4,24 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Lista de ##### </title>
+    <title>Lista de Canciones </title>
 </head>
 <body>
     
     <center>
     <table border=1 width="50%">
         <tr>
-            <td colspan=2><center><b> ##### </b</center></td>
+            <td colspan=2><center><b> Mis canciones </b</center></td>
         </tr>
 
         <tr>
             
-            <td> ##### </td>
+            <td> Hijo de la luna Mecano </td>
+            <td> A Dios le pido Juanes </td>
+            <td> No me doy por vencido Luis Fonsi </td>
             
         </tr>
 
         <tr>
-            <td colspan=2><center><b> ##### </b</center></td>
+            <td colspan=2><center><b> Quiz Primer corte </b</center></td>
         </tr>
     </table>
     </center>
